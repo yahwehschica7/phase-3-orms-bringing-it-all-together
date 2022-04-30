aaeb28e1375e146d0410b2d829296b1f434a361c
Jessica Nelson <yahwehschica@yahoo.com>
2022-04-30 11:01:51 -0400

    commit tree 48058411b28ca0fefcfc03195d8d79a70364a9ec

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,23 @@
 class Dog
+    attr_accessor :name, :breed, :id 
+
+    def initialize(name:, breed:, id: nil)
+        @name = name
+        @breed = breed
+        @id = id
+    end
+
+    def self.create_table
+        sql =  <<-SQL
+          CREATE TABLE IF NOT EXISTS dogs (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            breed TEXT
+            )
+        SQL
+        DB[:conn].execute(sql)
+      end
+
+
 
 end
